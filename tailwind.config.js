0e1abdcc41763cbbb509583bebfeeeec403ec876
Elmar Ismayilov <bamazy@gmail.com>
2023-08-24 09:51:23 +0800

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
     current: "currentColor",
     extend: {
       colors: {
+        primary: '#000000',
+        secondary: '#ecc94b',
+        background: '#fbfbfe',
+        danger: '#ef5959',
+        success: '#1dd15a',
+        blue: '#3861fb',
+        gray: '#c0c0c2',
+        dark: '#4b4b4d',
         // light mode
         tremor: {
           brand: {
